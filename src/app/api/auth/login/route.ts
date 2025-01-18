12bcab45b01ace9ec19b29ab9aa2ed9a922273e3
Fouad <fmtabbara@hotmail.co.uk>
2025-01-18 23:48:22 +0000

    Fix/auth updates (#21)
    * move nextauth to route
    
    * fix unnecessary auth routes
    
    * add prisma scripts
    
    * replace register api route with server action
    
    * set up example register and login
    
    * fix build errors

--- src/app/api/auth/login/route.ts
+++
@@ -1,8 +0,0 @@
-import { NextResponse } from "next/server";
-
-export async function POST() {
-  return NextResponse.json(
-    { message: "Logged in sucessfully" },
-    { status: 200 },
-  );
-}
---
+++ src/app/register/page.tsx
@@ -0,0 +1,61 @@
+"use client";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+import { RegisterUser } from "./action";
+
+const Register = () => {
+  const router = useRouter();
+  const [showPassword, setShowPassword] = useState(false);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = async () => {
+    const res = await RegisterUser({
+      email,
+      password,
+    });
+
+    if (res.status !== 201) {
+      return alert("Error creating user");
+    }
+
+    return router.push("/");
+  };
+  return (
+    <div className="p-24 flex flex-col gap-8 items-center">
+      <h2 className="font-semibold text-3xl">Register</h2>
+      <div className="flex flex-col gap-4">
+        <input
+          name="email"
+          className="p-1 rounded-md text-slate-900"
+          type="email"
+          placeholder="Email"
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <div className="flex flex-col gap-4">
+          <input
+            name="password"
+            className="p-1 rounded-md text-slate-900"
+            type={showPassword ? "text" : "password"}
+            placeholder="Password"
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <button type="button" onClick={() => setShowPassword(!showPassword)}>
+            {showPassword ? "Hide" : "Show"} password
+          </button>
+        </div>
+        <button
+          type="button"
+          onClick={handleSubmit}
+          className="p-2 rounded-md bg-primary text-white"
+        >
+          Register
+        </button>
+      </div>
+      <Link href="/api/auth/signin">Login</Link>
+    </div>
+  );
+};
+
+export default Register;
