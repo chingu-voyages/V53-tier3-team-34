fc5787d395240158ba0017a006b5456629a415a4
Tibardoe <tibardoebenjamin@gmail.com>
2025-01-28 12:58:25 +0000

    Fix deployment error

--- src/app/api/images/route.tsx
+++ src/app/api/images/route.tsx
@@ -3,16 +3,14 @@ import prisma from "../../../../prisma/client";
 
 export async function GET() {
   try {
-    try {
-      const allImages = await prisma.imageType.findMany();
+    const allImages = await prisma.imageType.findMany();
 
-      if (allImages) {
-        return NextResponse.json(allImages, { status: 200 });
-      }
-
-      return NextResponse.json({ message: "No images found" }, { status: 404 });
-    } catch (error) {
-      console.error(error);
+    if (allImages) {
+      return NextResponse.json(allImages, { status: 200 });
     }
-  } catch (error) {}
+
+    return NextResponse.json({ message: "No images found" }, { status: 404 });
+  } catch (error) {
+    console.error(error);
+  }
 }
