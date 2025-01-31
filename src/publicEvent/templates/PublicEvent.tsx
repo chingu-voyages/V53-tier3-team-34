6250e04bc899e77d3794374f22c6b2f460931dc1
muraliSingh7 <singhmurali.14@gmail.com>
2025-02-01 03:55:34 +0530

    Moving queryChipbar to src/publicEvent/organisms

--- src/publicEvent/templates/PublicEvent.tsx
+++ src/publicEvent/templates/PublicEvent.tsx
@@ -2,11 +2,13 @@ import type React from "react";
 import { Suspense } from "react";
 import Header from "../../home/organisms/Header";
 import EventSearchNavigation from "../organisms/EventSearchNavigation";
+import QueryChipBar from "../organisms/QueryChipBar";
 
 const PublicEvent: React.FC = () => {
   return (
     <main className="bg-black px-16 text-white min-h-screen">
       <Header />
+      <QueryChipBar />
       <Suspense fallback={<div>Loading events...</div>}>
         <EventSearchNavigation />
       </Suspense>
