5227ac85918822b74baf92d9f4e5d947139dffaf
Maryamh12 <maryam_hazrati94@yahoo.com>
2025-02-27 20:38:11 +0000

    Fix accessibility and style issuesmmsss

--- src/createEvent/templates/EventForm.tsx
+++ src/createEvent/templates/EventForm.tsx
@@ -329,6 +329,7 @@ const EventForm = () => {
             if (response?.eventId && formData.id === "0") {
               setFormData((prev) => ({ ...prev, id: response.eventId }));
             }
+            console.log("Id for response", response.eventId);
             console.log("Id for eventId", formData.id);
             // console.log("Chips data from eventPage", formData.chips);
             // console.log("Activity data from eventPage", formData.activity);
