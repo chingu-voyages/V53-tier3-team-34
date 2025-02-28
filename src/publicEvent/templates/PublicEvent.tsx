c1def2957d3b41f0caa575576a58348c76f906fa
muraliSingh7 <singhmurali.14@gmail.com>
2025-02-28 16:00:39 +0530

    Fixing issue

--- src/publicEvent/templates/PublicEvent.tsx
+++ src/publicEvent/templates/PublicEvent.tsx
@@ -1,4 +1,5 @@
 import Footer from "@/home/organisms/Footer";
+import Image from "next/image";
 import type React from "react";
 import { Suspense } from "react";
 import Header from "../../home/organisms/Header";
@@ -6,18 +7,36 @@ import Banner from "../organisms/Banner";
 import EventSearchNavigation from "../organisms/EventSearchNavigation";
 import QueryChipBar from "../organisms/QueryChipBar";
 
-const PublicEvent: React.FC = () => {
-  return (
-    <main className="bg-black px-16 text-white min-h-screen space-y-10">
-      <Header />
+const PublicEvent: React.FC = () => (
+  <main className="bg-black px-16 text-white min-h-screen space-y-10">
+    <Header />
+    <Suspense
+      fallback={
+        <Image
+          src="/assets/images/spinner.svg"
+          width={40}
+          height={40}
+          alt="Loading"
+        />
+      }
+    >
       <QueryChipBar />
-      <Banner />
-      <Suspense fallback={<div>Loading events...</div>}>
-        <EventSearchNavigation />
-      </Suspense>
-      <Footer />
-    </main>
-  );
-};
+    </Suspense>
+    <Banner />
+    <Suspense
+      fallback={
+        <Image
+          src="/assets/images/spinner.svg"
+          width={40}
+          height={40}
+          alt="Loading"
+        />
+      }
+    >
+      <EventSearchNavigation />
+    </Suspense>
+    <Footer />
+  </main>
+);
 
 export default PublicEvent;
